e68878a399f8302991c45ba81a7d74e891e18118
Ivan <txantxo1@gmail.com>
2015-05-01 18:18:03 -0400

    primera subida

---
+++ src/My/RecipesBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace My\RecipesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RecipesBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/My/RecipesBundle/RecipesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace My\RecipesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RecipesBundle extends Bundle
+{
+}
